37734801907624f397b51b1ece457091973deb72
ababa555 <japan14144@yahoo.co.jp>
2022-09-03 16:02:57 +0900

    Render.comにデプロイするための修正

--- src/pokemon_api/pokemon_api/settings/production.py
+++ src/pokemon_api/pokemon_api/settings/production.py
@@ -2,9 +2,10 @@ from pathlib import Path
 import dj_database_url
 from pokemon_api.settings.base import *
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 DEBUG = False
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
+DATABASES = {"default": dj_database_url.config()}
