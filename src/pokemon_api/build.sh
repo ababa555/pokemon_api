dbc76d2945398a839650835f86c337e0f6bd74d7
ababa555 <japan14144@yahoo.co.jp>
2022-09-17 22:36:00 +0900

    Render.comにデプロイするための修正

--- src/pokemon_api/build.sh
+++ src/pokemon_api/build.sh
@@ -2,6 +2,6 @@
 # exit on error
 set -o errexit
 
-/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
 cd ./src/pokemon_api
+/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
 pip install -r requirements.txt
